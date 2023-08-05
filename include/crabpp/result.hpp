50a15cdbef3a88dcd280a7130b276229e0ba0043
patrykstruzek <patrykstruzek17@gmail.com>
2023-08-05 19:35:31 +0200

    Improve result system

--- include/crabpp/result.hpp
+++ include/crabpp/result.hpp
@@ -1,118 +1,100 @@
 namespace crabpp
 {
-    template <typename OT, typename ET>
-    struct Result
+
+template <typename T, typename E>
+struct Result
+{
+    union Value
     {
-        union Value
-        {
-            OT ok;
-            ET err;
+        T ok;
+        E err;
 
-            Value() {}
-            ~Value() {}
-        };
-
-        enum class Tag
-        {
-            Ok,
-            Err
-        };
-
-        Tag tag;
-        Value value;
-
-        Result(const OT &data) : tag(Tag::Ok)
-        {
-            new (&value.ok) OT(data);
-        }
-
-        Result(const ET &error) : tag(Tag::Err)
-        {
-            new (&value.err) ET(error);
-        }
-
-        ~Result()
-        {
-            if (tag == Tag::Ok)
-                value.ok.~OT();
-            else
-                value.err.~ET();
-        }
-
-        Result(const Result &other) : tag(other.tag)
-        {
-            if (tag == Tag::Ok)
-                new (&value.ok) OT(other.value.ok);
-            else
-                new (&value.err) ET(other.value.err);
-        }
-
-        Result &operator=(const Result &other)
-        {
-            if (this == &other)
-                return *this;
-
-            this->~Result();
-
-            tag = other.tag;
-            if (tag == Tag::Ok)
-                new (&value.ok) OT(other.value.ok);
-            else
-                new (&value.err) ET(other.value.err);
-
-            return *this;
-        }
-
-        Result(Result &&other) noexcept : tag(other.tag)
-        {
-            if (tag == Tag::Ok)
-                new (&value.ok) OT(std::move(other.value.ok));
-            else
-                new (&value.err) ET(std::move(other.value.err));
-        }
-
-        Result &operator=(Result &&other) noexcept
-        {
-            if (this == &other)
-                return *this;
-
-            this->~Result();
-
-            tag = other.tag;
-            if (tag == Tag::Ok)
-                new (&value.ok) OT(std::move(other.value.ok));
-            else
-                new (&value.err) ET(std::move(other.value.err));
-
-            return *this;
-        }
-
-        bool is_ok() const { return tag == Tag::Ok; }
-        bool is_err() const { return tag == Tag::Err; }
-
-        explicit operator bool() const { return is_ok(); }
-
-        const T& ok() const { return okValue; }
-        const E& err() const { return errValue; }
-        
-        /*
-        FUNCTIONS TO DO:
-        is_ok_and
-        is_err_and
-        expect
-        expect_err
-        unwrap
-        unwrap_or_default
-        unwrap_err  
-        and
-        and_then
-        or
-        or_else
-        unwrap_or
-        unwrap_or_else
-
-        Err
-        panic   
-        */
+        Value() {}
+        ~Value() {}
     };
-}   
+
+    enum class Tag
+    {
+        Ok,
+        Err
+    };
+
+    Tag tag;
+    Value value;
+
+    Result(const T &data) : tag(Tag::Ok)
+    {
+        new (&value.ok) T(data);
+    }
+
+    Result(const E &error) : tag(Tag::Err)
+    {
+        new (&value.err) E(error);
+    }
+
+    ~Result()
+    {
+        if (tag == Tag::Ok)
+            value.ok.~T();
+        else
+            value.err.~E();
+    }
+
+    Result(const Result &other) : tag(other.tag)
+    {
+        if (tag == Tag::Ok)
+            new (&value.ok) T(other.value.ok);
+        else
+            new (&value.err) E(other.value.err);
+    }
+
+    Result &operator=(const Result &other)
+    {
+        if (this == &other)
+            return *this;
+
+        this->~Result();
+
+        tag = other.tag;
+        if (tag == Tag::Ok)
+            new (&value.ok) T(other.value.ok);
+        else
+            new (&value.err) E(other.value.err);
+
+        return *this;
+    }
+
+    Result(Result &&other) noexcept : tag(other.tag)
+    {
+        if (tag == Tag::Ok)
+            new (&value.ok) T(std::move(other.value.ok));
+        else
+            new (&value.err) E(std::move(other.value.err));
+    }
+
+    Result &operator=(Result &&other) noexcept
+    {
+        if (this == &other)
+            return *this;
+
+        this->~Result();
+
+        tag = other.tag;
+        if (tag == Tag::Ok)
+            new (&value.ok) T(std::move(other.value.ok));
+        else
+            new (&value.err) E(std::move(other.value.err));
+
+        return *this;
+    }
+
+    bool is_ok() const { return tag == Tag::Ok; }
+    bool is_err() const { return tag == Tag::Err; }
+
+    explicit operator bool() const { return is_ok(); }
+
+    const T& unwrap() const { return value.ok; }
+    const E& unwrap_err() const { return value.err; } 
+};
+
+}
--- tests/main.cpp
+++ tests/main.cpp
@@ -1,16 +1,14 @@
 #include <crabpp/option.hpp>
+#include <crabpp/result.hpp>
 
 #include <iostream>
 
 using namespace crabpp;
 
+
+
 int main()
 {
-    int i = 1;
 
-    match(i,
-        {
-        {1, []() { std::cout << "Yes" << std::endl; }},
-        {2, []() { std::cout << "No" << std::endl; }}
-    });
+
 }
