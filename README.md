67b2a87bea2eb4caeb2d1e4f6dad848f13e3051a
Patryk Strużek <patrykstruzek17@gmail.com>
2024-01-15 21:00:46 +0100

    Improve result and delete matcher

--- README.md
+++ README.md
@@ -24,51 +24,24 @@ or just add it through git submodule.
 
 ## Examples
 
-### Result
 ```cpp
 enum ErrorCode {
     SomeError
 };
 
-auto foo(int x) -> Result<std::string, ErrorCode> {
-    if(x > 10)
-    {
-        return Err<std::string, ErrorCode>(ErrorCode::SomeError);
-    }
+auto greater_than_nine(int x) -> Result<std::string, ErrorCode> {
+    if(x <= 9)
+        return Err(ErrorCode::SomeError);
 
-    return Ok<std::string, ErrorCode>("The number is ok!");
+    return Ok("The number is ok!");
 }
 
 auto result = foo(11);
 if(!result) {
-std::cerr << "Some error occured!";
+    std::cerr << "Some error occured!";
 } else {
-std::cout << result.value();
+    std::cout << result.value();
 }
 ```
-
-### match
-```cpp
-std::string input = "some_text";
-
-int result = match<std::string, int>(input)
-                    .with("some_text", []() { return 0; })
-                    .with("not_some_text", []() { return 1; })
-                    .with("some_other_text", []() { return 2; })
-                    .rest([]() { return NULL; });
-```
-
-## Simple docs
-- <strong>Result<T, E></strong> - main result class
-- .err() - returns an error 
-- .value() - returns a value
-- .unwrap() - returns a value if ok, if not ok throw exception
-- .expect() - if not ok throw exception with message in arguments
-- <strong>Ok<T, E>()</strong> - returns an ok value
-- <strong>Err<T, E>()</strong> - returns an error
-- <strong>match<T, R>()</strong> - main match func
-- .with() - compare with value from match arguments and return value
-- .rest() - checks the rest cases and return a value
-
 ---
 Take into account that this library has no real tests, so it is not perfect.
---
+++ result.hpp
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <stdexcept>
+#include <utility>
+#include <variant>
+
+namespace options {
+    template <typename T> struct [[nodiscard]] Ok {
+        Ok(const T& val) : val(val) {}
+        Ok(T&& val) : val(std::move(val)) {}
+
+        T val;
+    };
+
+    template <> struct Ok<void> {};
+
+    template <typename E> struct [[nodiscard]] Err {
+        Err(const E& err) : err(err) {}
+        Err(E&& val) : err(std::move(val)) {}
+
+        E err;
+    };
+}
+
+template <typename T, typename E> class [[nodiscard]] Result {
+private:
+    std::variant<options::Ok<T>, options::Err<E>> value;
+
+public:
+    Result(options::Ok<T> o) : value(std::move(o)) {}
+    Result(options::Err<E> e) : value(std::move(e)) {}
+
+    [[nodiscard]] bool is_ok() const noexcept {
+        return std::holds_alternative<options::Ok<T>>(value);
+    }
+
+    [[nodiscard]] bool is_err() const noexcept {
+        return !is_ok();
+    }
+
+    [[nodiscard]] T unwrap() {
+        if (is_ok())
+            return std::move(std::get<options::Ok<T>>(value).val);
+        else
+            throw std::logic_error("Result contains an error (unwrap)");
+    }
+
+    template <typename F>
+    [[nodiscard]] E expect(F&& msg) {
+        if (!is_ok())
+            throw std::logic_error(std::forward<F>(msg)(std::get<options::Err<E>>(value).err));
+        else
+            throw std::logic_error("Result does not contain an error (expect)");
+    }
+
+    explicit operator bool() const noexcept {
+        return is_ok();
+    }
+};
+
+template <typename T, typename CleanT = typename std::decay<T>::type>
+[[nodiscard]] options::Ok<CleanT> Ok(T&& val) {
+    return options::Ok<CleanT>(std::forward<T>(val));
+}
+
+inline options::Ok<void> Ok() {
+    return options::Ok<void>();
+}
+
+template <typename E, typename CleanE = typename std::decay<E>::type>
+[[nodiscard]] options::Err<CleanE> Err(E&& val) {
+    return options::Err<CleanE>(std::forward<E>(val));
+}
